b133d6fdbabe20874ec55bbaf19ef0a774f52cf4
Emily Wilbur <ewilbur94@gmail.com>
2021-03-30 00:35:45 -0400

    displaying Api results in result section//attemping to add date but it looks like I need to convert(still attempting//Can't find UV index in Api

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,21 @@
 // variable declaration
 var searchForm = $("#search-form");
 var searchCityEl = $("#search-city");
+var resultSection = document.getElementById("result-section");
 
 
+
+// displaying Api results in result section
+function displayWeather(data) {
+    // var temp = data.main.temp;
+    // var humidity = data.main.humidity;
+    // var windSpeed = data.wind.speed;
+    
+    $("#cityName").append(data.name);
+    $("#tempLabel").append("Temperature: " + data.main.temp + "°F");
+    $("#humidityLabel").append("Humidity: " + data.main.humidity + "%");
+    $("#windSpeedLabel").append("Wind Speed: " + data.wind.speed + "mph");
+}
 
 
 
@@ -23,15 +36,21 @@ searchForm.on("submit", function(event){
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&units=imperial&appid=" + apiKey;
 
+
     fetch(queryURL)
     .then(function(response){
         return response.json();
     })
     .then(function(data) {
+        // console.log(data.main.temp);
+        // console.log(data.main.humidity);
         console.log(data);
+        console.log(data.wind.speed);
+        displayWeather(data);
     });
-
-
 });
 
 
+function convertDate(currentDate) {
+    return moment.unix(currentDate).format('MM/DD/YYYY');
+}
